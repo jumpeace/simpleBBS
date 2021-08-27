d4c312345f9c8245e355299d52bd2463496e750c
jpig-com <jumpei141107@gmail.com>
2021-08-27 22:41:33 +0900

    CLEAN: models.rbの共通部分をhelper/app/models.rbにまとめた

--- app/reply/models.rb
+++ app/reply/models.rb
@@ -1,7 +1,4 @@
-require 'active_record'
-
-ActiveRecord::Base.configurations = YAML.load_file('./config/database.yml')
-ActiveRecord::Base.establish_connection :development
+require './helper/app/models'
 
 class Reply < ActiveRecord::Base
 end
---
+++ helper/app/models.rb
@@ -0,0 +1,4 @@
+require 'active_record'
+
+ActiveRecord::Base.configurations = YAML.load_file('./config/database.yml')
+ActiveRecord::Base.establish_connection :development
